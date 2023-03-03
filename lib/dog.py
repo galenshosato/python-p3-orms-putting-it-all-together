903efd58df0b2268082e6beee9543bbc7aebdd46
Galen Sho Sato <galen.sato@gmail.com>
2023-03-02 20:40:07 -0500

    Completed Lab

--- lib/dog.py
+++ lib/dog.py
@@ -29,4 +29,80 @@ class Dog:
         CURSOR.execute(sql)
     
     def save(self):
-        pass
+        sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.breed))
+        CONN.commit()
+
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM dogs").fetchone()[0]
+
+    @classmethod
+    def create (cls, name, breed):
+        dog = Dog(name, breed)
+        dog.save()
+        return dog
+    
+    @classmethod
+    def new_from_db (cls, row):
+        dog = cls(row[1], row[2])
+        dog.id = row[0]
+        return dog
+    
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM dogs
+        """
+        all = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.new_from_db(row) for row in all]
+        return cls.all
+    
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM dogs
+            WHERE dogs.name = ?
+        """
+
+        name_object = CURSOR.execute(sql, (name,)).fetchone()
+        if name_object == None:
+            return None
+        else:
+            dog = cls.new_from_db(name_object)
+            return dog
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT *
+            FROM dogs
+            WHERE dogs.id = ?
+        """
+
+        id_object = CURSOR.execute(sql, (id,)).fetchone()
+        id = cls.new_from_db(id_object)
+        return id
+    
+    @classmethod
+    def find_or_create_by(cls, name, breed):
+        dog = cls.find_by_name(name)
+        if dog == None:
+            new_dog = cls.create(name, breed)
+            return new_dog
+        else:
+            return dog
+
+
+    def update(self):
+        sql = """
+            UPDATE dogs
+            SET name = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.id))
+        CONN.commit()
